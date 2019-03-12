fc6a6bbdf59461d86a87bd8f67fe4e8377c5e370
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2019-03-13 04:14:39 +0700

    Updated

---
+++ app/src/main/java/com/e_feels/e_feelsmanager/WifiListActivity.java
@@ -0,0 +1,155 @@
+package com.e_feels.e_feelsmanager;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WifiListActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button buttonScan;
+    private int size = 0;
+    private List<ScanResult> results;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi_list);
+
+        // Menampilkan Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Menampilkan Title pada Toolbar
+        TextView toolbarText = (TextView) findViewById(R.id.toolbar_text);
+        if(toolbarText!=null && toolbar!=null) {
+            toolbarText.setText("Connect to Network Device");
+            setSupportActionBar(toolbar);
+        }
+
+        // Minta akses Location agar mau tampil list wifi nya
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+
+        }
+
+        buttonScan = findViewById(R.id.btn_scan_wifi);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+            }
+        });
+
+        listView = findViewById(R.id.lv_wifi_list);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this, "WiFi is disabled... We need to enable it", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        scanWifi();
+
+        // listening to single list item on click
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // selected item
+                String ssid = ((TextView) view).getText().toString();
+                connectToWifi(ssid);
+                Toast.makeText(WifiListActivity.this, ssid, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void scanWifi() {
+        arrayList.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning WiFi...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void connectToWifi(final String wifiSSID) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_wifi_connect);
+        dialog.setTitle("Connect to Network");
+        TextView textSSID = (TextView) dialog.findViewById(R.id.tv_ssid);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.btn_conn_wifi);
+        EditText pass = (EditText) dialog.findViewById(R.id.et_wifi_password);
+        textSSID.setText(wifiSSID);
+
+        // if button is clicked, connect to the network;
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String checkPassword = pass.getText().toString();
+                finallyConnect(checkPassword, wifiSSID);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void finallyConnect(String networkPass, String networkSSID) {
+        WifiConfiguration wifiConfig = new WifiConfiguration();
+        wifiConfig.SSID = String.format("\"%s\"", networkSSID);
+        wifiConfig.preSharedKey = String.format("\"%s\"", networkPass);
+
+        // remember id
+        int netId = wifiManager.addNetwork(wifiConfig);
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
+
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = "\"\"" + networkSSID + "\"\"";
+        conf.preSharedKey = "\"" + networkPass + "\"";
+        wifiManager.addNetwork(conf);
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results) {
+                int level = WifiManager.calculateSignalLevel(scanResult.level, 5);
+                arrayList.add(scanResult.SSID + " - Signal: " + level);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
+}
